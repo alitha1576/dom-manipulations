d4869b882158ad667bfa6e0757c57351d5fbfff5
Anna Petrova <ann.undine@gmail.com>
2024-11-20 15:22:11 +0100

    second exercise

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//// first exercise
+
 const counterField = document.getElementById('counter');
 let counter = 0;
 
@@ -9,7 +11,27 @@ plusButton.addEventListener('click', function() {
     counterField.innerHTML = counter;
 });
 
-minus.addEventListener('click', function() {
+minusButton.addEventListener('click', function() {
     counter -= 1;
     counterField.innerHTML = counter;
+});
+
+//// second exercise
+
+const counterField2 = document.getElementById('counter2');
+let counter2 = 0;
+
+const plusButton2 = document.getElementById('plus2');
+const minusButton2 = document.getElementById('minus2')
+
+plusButton2.addEventListener('click', function() {
+    counter2 += 1;
+    counterField2.innerHTML = counter2;
+});
+
+minusButton2.addEventListener('click', function() {
+    if (counter2 > 0) {
+        counter2 -= 1;
+    counterField2.innerHTML = counter2;
+    }
 });
